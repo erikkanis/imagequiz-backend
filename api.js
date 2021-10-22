a7e6208a9c7ba81194cf58069d0a0979334ef0bf
Erik Kanis <erikkanis@email.arizona.edu>
2021-10-22 01:45:47 -0700

    Practicum completed?

--- api.js
+++ api.js
@@ -1,4 +1,7 @@
 var { customers } = require('./data_tier/customers');
+var { flowers } = require('./data_tier/flowers');
+var { quizzes } = require('./data_tier/data');
+var { scores } = require('./data_tier/scores');
 
 let add = (n, m) => {
     return n + m;
@@ -8,10 +11,58 @@ let sub = (n, m) => {
     return n - m;
 }
 
-let addCustomer = (name, email, password) => {
-    customers.push({id: customers.length + 1, name, email, password});
+let getFlowers = () => {
+    let flowerName=[];
+    for(let i = 0; i < flowers.length; i++){
+        flowerName.push(flowers[i].name);
+    }
+    return flowerName;
 }
 
+let getQuizzes = () => {
+    return quizzes;
+}
+
+let getQuizID = (id) => {
+    return quizzes[id];
+}
+
+let addCustomer = (name, email, password) => {
+    let alreadyExist = customers.find(x=> x.email.toLowerCase() === email.toLowerCase());
+    if(alreadyExist){
+        return true;
+    }
+    customers.push({id: customers.length + 1, name: name, email: email, password: password});
+    return false;
+}
+
+let customerLogin = ( email, password) => {
+    let isValid = customers.find(x => x.email.toLowerCase() === email.toLowerCase() && x.password == password);
+    if(isValid){
+        return true;
+    }
+    return false;
+}
+
+let addScore = (quizTaker, quizID, score) => {
+    scores.push({quizTaker, quizID, score});
+}
+
+let checkUserScore = (quizTaker, quizID) => {
+    for(let i = 0; i < scores.length; i++ ){
+        if(scores[i] === quizTaker && scores[i].quizID === quizID) {
+            return scores[i].score;
+        }
+    }
+    return -1;
+}
+
+exports.checkUserScore = checkUserScore;
+exports.addScore = addScore;
+exports.getQuizID = getQuizID;
+exports.getQuizzes = getQuizzes;
+exports.getFlowers = getFlowers;
+exports.customerLogin = customerLogin;
 exports.addCustomer = addCustomer;
 exports.add = add;
 exports.sub = sub;
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const api = require('./api');
+const { scores } = require('./data_tier/scores');
 
 const application = express();
-const port = process.env.PORT || 4005;
+const port = process.env.PORT || 4002;
+
+application.use(express.json());
 
 //add test function
 application.get('/add/:n/:m', (request, response) => {
@@ -23,7 +26,52 @@ application.post('/register', (request, response) => {
     let name = request.body.name;
     let email = request.body.email;
     let password = request.body.password;
-    api.addCustomer(name, email, password);
+    let alreadyExists = api.addCustomer(name, email, password);
+    if(alreadyExists) {
+        response.status(403).json({message: 'A customer with the same email already exists'});
+    } else {
+        response.json({message: 'The customer was added.'});
+    }
+});
+
+application.post('/login', (request, response) => {
+    let email = request.body.email;
+    let password = request.body.password;
+    let isValid = api.customerLogin(email, password);
+    if(isValid) {
+        response.json({message: 'Login successful'});
+    } else {
+        response.status(404).json({message: 'User not found'});
+    }
+});
+
+application.get('/flowers', (request, response) => {
+    let flowerName = api.getFlowers();
+    response.send(JSON. stringify(flowerName));
+});
+
+application.get('/quizzes', (request, response) => {
+    let quizQuestions = api.getQuizzes();
+    response.send(JSON. stringify(quizQuestions));
+});
+
+application.get('/quiz/:id', (request, response) => {
+    let quizID = api.getQuizID(request.params.id);
+    response.send(JSON. stringify(quizID));
+});
+
+application.post('/score', (request, response) => {
+    api.addScore(request.body.quizTaker, request.body.quizID, request.body.score);
+    response.send(JSON. stringify({message: "Score was added"}));
+});
+
+application.get('/scores/:quiztaker/:quizid', (request, response) => {
+    let quizScore = api.checkUserScore(request.body.quizTaker, request.body.quizID);
+    if(quizScore == -1) {
+        response.status(404).json({message: 'User not found'});
+    } else {
+        response.send(JSON. stringify(quizScore));
+    }
 });
 
 application.listen(port, () => console.log('Listening on port ' + port));
