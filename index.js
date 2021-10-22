cdb9ef4137c9509dfc95329bc1d41ac5403e6dea
Erik Kanis <erikkanis@email.arizona.edu>
2021-10-21 23:43:13 -0700

    comment

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const api = require('./api');
 const application = express();
 const port = process.env.PORT || 4005;
 
+//add test function
 application.get('/add/:n/:m', (request, response) => {
     let n = Number(request.params.n);
     let m = Number(request.params.m);
