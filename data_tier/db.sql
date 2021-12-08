4dadf65fe2611b91c69c039eff3134d631056d34
Erik Kanis <erikkanis@email.arizona.edu>
2021-12-08 01:15:37 -0700

    fix

--- data_tier/db.sql
+++ data_tier/db.sql
@@ -1,5 +1,7 @@
 create schema if not exists imagequiz;
 
+drop table if exists imagequiz.flower;
+drop table if exists imagequiz.score;
 drop table if exists imagequiz.quiz_question cascade;
 drop table if exists imagequiz.quiz cascade;
 drop table if exists imagequiz.question cascade;
@@ -15,6 +17,13 @@ create table imagequiz.customer (
     password text not null
 );
 
+create table imagequiz.flower
+(
+    id bigserial primary key,
+    name text not null,
+    picture text not null
+);
+
 create table imagequiz.question (
     id bigserial primary key,
     picture text not null,
@@ -27,10 +36,19 @@ create table imagequiz.category (
     name text not null
 );
 
-create table iamgequiz.quiz (
+create table imagequiz.quiz (
     id bigserial primary key,
     name text not null,
     category_id int not null REFERENCES imagequiz.category(id)
+);
+
+create table imagequiz.score
+(
+    id bigserial primary key,
+    customer_id int not null references imagequiz.customer(id),
+    quiz_id int not null references imagequiz.quiz(id),
+    score int not null,
+    date timestamp not null default now()
 );
 
 create table imagequiz.quiz_question (
